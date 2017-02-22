bc5ea9dfe676f36c8d074c48aa58e5afb5e4bb3c
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-02-22 19:25:46 +0200

    feat: App API Routes

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 const express = require('express');
-const authAPIv1 = require('./app/routes/api/v1/auth');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const passport = require('passport');
 
+
+const authMiddleware = require('./app/middlewares/authMiddleware');
+const authAPIv1 = require('./app/routes/api/v1/auth');
 const app = express();
 
+passport.use(authMiddleware.strategy);
+app.use(passport.initialize());
+
+
+
+/**
+ * API ROUTES
+ */
 app.use('/api/v1/auth', authAPIv1);
+
+/**
+ * Error Handling Middlewares.
+ */
+
+app.use(function (err, req, res, next) {
+    return res.status(500).json({
+        message: err.toString()
+    });
+});
+
 module.exports = app;
