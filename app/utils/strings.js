2580c0e00f09c712a8904ca952a6e423ce62dc76
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-02-22 20:18:36 +0200

    feat: Unified String File

---
+++ app/utils/strings.js
@@ -0,0 +1,11 @@
+const Strings = {
+    INCOMPLETE_INFORMATION: 'Please fill out all the required information.',
+    NON_GUC_MAIL: 'Please enter a Valid GUC mail.',
+    PASSWORD_MISMATCH: 'Password and password confirmation mismatch.',
+    INVALID_PASSWORD: 'The password must be at least 8 characters and includes at least a digit',
+    INVALID_GUC_ID: 'Please enter a Valid GUC ID',
+
+    SIGNUP_SUCCESS: 'Signed Up Successfully'
+}
+
+module.exports = Strings;
