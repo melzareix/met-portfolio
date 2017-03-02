bd44be58ffec35d4189ff5d9dd9388f3df29f119
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-03-02 12:31:40 +0200

    refactor: Replace usage of var

--- app/routes/api/v1/portfolio.js
+++ app/routes/api/v1/portfolio.js
@@ -4,10 +4,10 @@ const authHelper = require('../../../middlewares/authMiddleware');
 const multer = require('multer');
 const Strings = require('../../../utils/strings');
 const path = require('path');
+const crypto = require('crypto');
 const WorkItem = require('../../../models/WorkItem');
 const User = require('../../../models/User');
 const Tag = require('../../../models/Tag');
-const crypto = require('crypto');
 
 const router = express.Router();
 require('dotenv').config();
@@ -306,7 +306,7 @@ let createTags = (tags, cb) => {
  */
 
 let validateUrl = function (url) {
-    var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
+    const regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
     return regex.test(url);
 };
 
